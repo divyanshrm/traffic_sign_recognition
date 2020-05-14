e15d91f26d9921fa6822704a5df1034f7b1264fb
divyanshrm <43374952+divyanshrm@users.noreply.github.com>
2020-05-15 02:39:42 +0530

    Add files via upload

---
+++ preprocess.py
@@ -0,0 +1,8 @@
+from skimage import exposure
+import numpy as np
+def preprocess(data)
+    data=np.reshape(data,(75,75))
+    data=exposure.equalize_hist(data)
+    data=data255
+    data=np.reshape(data,(75,75,1))
+    return data
